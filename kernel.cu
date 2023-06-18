5337acef6b2d661460ab57911e7a53d2358ffe35
ushiostarfish <ushiostarfish@gmail.com>
2023-06-18 18:18:49 +0900

    add prefix scan

--- kernel.cu
+++ kernel.cu
@@ -1,4 +1,114 @@
-extern "C" __global__ void kernelMain()
-{
+typedef unsigned long long uint64_t;
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
 
+#define RADIX_SORT_BLOCK_SIZE 1024
+#define RADIX_SORT_PREFIX_SCAN_BLOCK 256
+
+__device__ inline int div_round_up( int val, int divisor )
+{
+	return ( val + divisor - 1 ) / divisor;
 }
+
+extern "C" __global__ void blockCount( uint64_t* inputs, uint64_t numberOfInputs, uint32_t* counters, uint32_t bitLocation )
+{
+	__shared__ uint32_t localCounters[256];
+	if( threadIdx.x < 256 )
+	{
+		localCounters[threadIdx.x] = 0;
+	}
+	__syncthreads();
+
+	uint64_t itemIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	if( itemIndex < numberOfInputs )
+	{
+		uint64_t item = inputs[itemIndex];
+		uint32_t bits = ( item >> bitLocation ) & 0xFF;
+		atomicInc( &localCounters[bits], 0xFFFFFFFF );
+	}
+
+	__syncthreads();
+
+	if( threadIdx.x < 256 )
+	{
+		// column major: 
+		int numberOfBlocks = div_round_up( numberOfInputs, RADIX_SORT_BLOCK_SIZE );
+		int bucketIndex = threadIdx.x;
+		int blockIndex = blockIdx.x;
+		uint64_t counterIndex = bucketIndex * numberOfBlocks + blockIndex; 
+		counters[counterIndex] = localCounters[bucketIndex];
+	}
+}
+
+__device__ void warpPrefixSumExclusive( uint32_t val, uint32_t* p, uint32_t* sum )
+{
+	uint32_t x = val;
+
+	for( uint32_t offset = 1; offset < 32; offset <<= 1 )
+	{
+		__syncwarp( 0xffffffff );
+		uint32_t y = __shfl_up_sync( 0xffffffff, x, offset );
+
+		if( offset <= threadIdx.x )
+		{
+			x += y;
+		}
+	}
+
+	__syncwarp( 0xffffffff );
+	*sum = __shfl_sync( 0xffffffff, x, 31 );
+	*p = x - val;
+}
+
+
+extern "C" __global__ void prefixSumExclusive( uint32_t* inputs, uint64_t numberOfInputs, uint32_t* sums, uint32_t* iterator, uint32_t* globalPrefix )
+{
+	//uint32_t r;
+	//uint32_t s;
+	//warpPrefixSumExclusive( 1, &r, &s );
+	//printf( "[%d] %d %d\n", threadIdx.x, r, s );
+
+	//assert( 0 );
+	// printf( "%d %d\n", threadIdx.x, __shfl_up_sync( 0xffffffff, threadIdx.x, 1 ) );
+	// printf( "%d %d\n", threadIdx.x, warpPrefixSum( 2 ) );
+
+	__shared__ uint32_t localPrefixSum[RADIX_SORT_PREFIX_SCAN_BLOCK];
+
+	int blockIndex = blockIdx.x;
+	int prefix = 0;
+	for( int i = 0; i < RADIX_SORT_PREFIX_SCAN_BLOCK; i += 32 )
+	{
+		int itemIndex = blockIndex * RADIX_SORT_PREFIX_SCAN_BLOCK + i + threadIdx.x;
+		uint32_t p;
+		uint32_t s;
+		warpPrefixSumExclusive( inputs[itemIndex], &p, &s );
+		localPrefixSum[i + threadIdx.x] = prefix + p;
+		prefix += s;
+	}
+
+	int gp;
+	if( threadIdx.x == 0 )
+	{
+		while( atomicAdd( iterator, 0 ) != blockIndex )
+			;
+
+		__threadfence();
+
+		gp = atomicAdd( globalPrefix, prefix );
+
+		__threadfence();
+
+		atomicInc( iterator, 0xFFFFFFFF );
+	}
+
+	__syncwarp( 0xffffffff );
+	gp = __shfl_sync( 0xffffffff, gp, 0 );
+
+	for( int i = 0; i < RADIX_SORT_PREFIX_SCAN_BLOCK; i += 32 )
+	{
+		int itemIndex = blockIndex * RADIX_SORT_PREFIX_SCAN_BLOCK + i + threadIdx.x;
+		sums[itemIndex] = gp + localPrefixSum[i + threadIdx.x];
+	}
+}
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include "shader.hpp"
 
+#include <stdint.h>
+
+struct splitmix64
+{
+	uint64_t x = 0; /* The state can be seeded with any value. */
+
+	uint64_t next()
+	{
+		uint64_t z = ( x += 0x9e3779b97f4a7c15 );
+		z = ( z ^ ( z >> 30 ) ) * 0xbf58476d1ce4e5b9;
+		z = ( z ^ ( z >> 27 ) ) * 0x94d049bb133111eb;
+		return z ^ ( z >> 31 );
+	}
+};
+inline int div_round_up( int val, int divisor )
+{
+	return ( val + divisor - 1 ) / divisor;
+}
+
+#define RADIX_SORT_BLOCK_SIZE 1024
+#define RADIX_SORT_PREFIX_SCAN_BLOCK 256
+
 int main()
 {
 	if( oroInitialize( (oroApi)( ORO_API_HIP | ORO_API_CUDA ), 0 ) )
@@ -31,10 +53,88 @@ int main()
 		std::string baseDir = "../"; /* repository root */
 		Shader shader( ( baseDir + "\\kernel.cu" ).c_str(), "kernel.cu", { baseDir }, {}, CompileMode::RelwithDebInfo, isNvidia );
 
-		ShaderArgument args;
-		shader.launch( "kernelMain", args, 1, 1, 1, 1, 32, 1, stream );
+		std::vector<uint64_t> inputs( 1024 * 2 );
+
+		splitmix64 rng;
+		for( int i = 0; i < inputs.size(); i++ )
+		{
+			inputs[i] = rng.next();
+		}
+
+		uint64_t numberOfInputs = inputs.size();
+		uint64_t numberOfBlocks = div_round_up( numberOfInputs, RADIX_SORT_BLOCK_SIZE );
+
+		Buffer inputsBuffer( sizeof( uint64_t ) * inputs.size() );
+
+		// column major
+		// +---- buckets ( 256 ) ----
+		// |
+		// blocks
+		Buffer counterBuffer( sizeof( uint32_t ) * 256 * numberOfBlocks );
+		Buffer counterPrefixSumBuffer( sizeof( uint32_t ) * 256 * numberOfBlocks );
+		Buffer prefixSumIteratorBuffer( sizeof( uint32_t ) );
+		Buffer globalPrefixBuffer( sizeof( uint32_t ) );
+
+		oroMemcpyHtoDAsync( (oroDeviceptr)inputsBuffer.data(), inputs.data(), sizeof( uint64_t ) * inputs.size(), stream );
+		oroMemsetD32Async( (oroDeviceptr)prefixSumIteratorBuffer.data(), 0, 1, stream );
+		oroMemsetD32Async( (oroDeviceptr)globalPrefixBuffer.data(), 0, 1, stream );
 
 		oroStreamSynchronize( stream );
+
+		{
+			ShaderArgument args;
+			args.add( inputsBuffer.data() );
+			args.add( numberOfInputs );
+			args.add( counterBuffer.data() );
+			args.add( 0 );
+			shader.launch( "blockCount", args, numberOfBlocks, 1, 1, RADIX_SORT_BLOCK_SIZE, 1, 1, stream );
+		}
+
+		// Prefix Sum 
+		{
+			ShaderArgument args;
+			args.add( counterBuffer.data() );
+			args.add( numberOfBlocks * 256 );
+			args.add( counterPrefixSumBuffer.data() );
+			args.add( prefixSumIteratorBuffer.data() );
+			args.add( globalPrefixBuffer.data() );
+			// shader.launch( "prefixSumExclusive", args, 1, 1, 1, 32, 1, 1, stream );
+			shader.launch( "prefixSumExclusive", args, numberOfBlocks * 256 / RADIX_SORT_PREFIX_SCAN_BLOCK, 1, 1, 32, 1, 1, stream );
+		}
+
+		oroStreamSynchronize( stream );
+
+
+
+		//std::vector<uint32_t> counterBufferRef( 256 * numberOfBlocks );
+		//for( int i = 0; i < inputs.size(); i += RADIX_SORT_BLOCK_SIZE )
+		//{
+		//	for( int j = 0; j < RADIX_SORT_BLOCK_SIZE; j++ )
+		//	{
+		//		int bitLocation = 0;
+		//		uint64_t item = inputs[i + j];
+		//		uint32_t bits = ( item >> bitLocation ) & 0xFF;
+
+		//		int bucketIndex = bits;
+		//		int blockIndex = i / RADIX_SORT_BLOCK_SIZE;
+		//		uint64_t counterIndex = bucketIndex * numberOfBlocks + blockIndex;
+		//		counterBufferRef[counterIndex]++;
+		//	}
+		//}
+
+		std::vector<uint32_t> counterBufferFromGPU( 256 * numberOfBlocks );
+		oroMemcpyDtoH( counterBufferFromGPU.data(), (oroDeviceptr)counterBuffer.data(), sizeof( uint32_t ) * 256 * numberOfBlocks );
+
+		//for (int i = 0; i < counterBufferRef.size(); i++)
+		//{
+		//	SH_ASSERT( counterBufferRef[i] == counterBufferFromGPU[i] );
+		//}
+
+
+		std::vector<uint32_t> counterPrefixSumBufferFromGPU( 256 * numberOfBlocks );
+		oroMemcpyDtoH( counterPrefixSumBufferFromGPU.data(), (oroDeviceptr)counterPrefixSumBuffer.data(), sizeof( uint32_t ) * 256 * numberOfBlocks );
+
+		printf( "counterPrefixSumBufferFromGPU" );
 	}
 
 	oroCtxDestroy( ctx );
